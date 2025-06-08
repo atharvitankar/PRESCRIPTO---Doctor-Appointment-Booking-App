0039ccc1421cfff11d59d1cfc5dd372ebd0f71ae
Atharv Itankar <123641223+atharvitankar@users.noreply.github.com>
2025-06-08 14:55:39 +0530

    Update Mail.js

--- backend/utils/Mail.js
+++ backend/utils/Mail.js
@@ -17,20 +17,3 @@ export function sendMail(to,sub,msg){
 };
 
 
-
-
-
-// var mailOptions = {
-//   from: 'youremail@gmail.com',
-//   to: 'myfriend@yahoo.com',
-//   subject: 'Appointment status',
-//   text: 'Your appointment is confirmed.'
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
