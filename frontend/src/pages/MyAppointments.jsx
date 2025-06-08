d7096542250791a8d3a8fe51e6e4178a99e0cf40
Atharv Itankar <123641223+atharvitankar@users.noreply.github.com>
2025-06-08 14:53:22 +0530

    Update MyAppointments.jsx

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -55,68 +55,6 @@ const MyAppointments = () => {
 
     }
 
-    // const initPay = (order) => {
-    //     const options = {
-    //         key: import.meta.env.VITE_RAZORPAY_KEY_ID,
-    //         amount: order.amount,
-    //         currency: order.currency,
-    //         name: 'Appointment Payment',
-    //         description: "Appointment Payment",
-    //         order_id: order.id,
-    //         receipt: order.receipt,
-    //         handler: async (response) => {
-
-    //             console.log(response)
-
-    //             try {
-    //                 const { data } = await axios.post(backendUrl + "/api/user/verifyRazorpay", response, { headers: { token } });
-    //                 if (data.success) {
-    //                     navigate('/my-appointments')
-    //                     getUserAppointments()
-    //                 }
-    //             } catch (error) {
-    //                 console.log(error)
-    //                 toast.error(error.message)
-    //             }
-    //         }
-    //     };
-    //     const rzp = new window.Razorpay(options);
-    //     rzp.open();
-    // };
-
-    // // Function to make payment using razorpay
-    // const appointmentRazorpay = async (appointmentId) => {
-    //     try {
-    //         const { data } = await axios.post(backendUrl + '/api/user/payment-razorpay', { appointmentId }, { headers: { token } })
-    //         if (data.success) {
-    //             initPay(data.order)
-    //         }else{
-    //             toast.error(data.message)
-    //         }
-    //     } catch (error) {
-    //         console.log(error)
-    //         toast.error(error.message)
-    //     }
-    // }
-
-    // // Function to make payment using stripe
-    // const appointmentStripe = async (appointmentId) => {
-    //     try {
-    //         const { data } = await axios.post(backendUrl + '/api/user/payment-stripe', { appointmentId }, { headers: { token } })
-    //         if (data.success) {
-    //             const { session_url } = data
-    //             window.location.replace(session_url)
-    //         }else{
-    //             toast.error(data.message)
-    //         }
-    //     } catch (error) {
-    //         console.log(error)
-    //         toast.error(error.message)
-    //     }
-    // }
-
-
-
     useEffect(() => {
         if (token) {
             getUserAppointments()
